52e23d8f5b335f59869afd37c3ecdc314b8d372d
Yash-Kumar-19 <kyash0000@gmail.com>
2022-07-06 13:53:42 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -8,16 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-// mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/group24Database?retryWrites=true&w=majority"
-// ,{useNewUrlParser: true})
-// .then(()=>console.log('MongoDB is connected'))
-// .catch(err => console.log(err));
+mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/group24Database?retryWrites=true&w=majority"
+,{useNewUrlParser: true})
+.then(()=>console.log('MongoDB is connected'))
+.catch(err => console.log(err));
 
-mongoose.connect("mongodb+srv://Yash1999:3Bw0gG3jLVVmLcCb@cluster0.0pxxn.mongodb.net/proj3", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+
 
 app.use('/',route);
 app.listen(process.env.PORT || 3000, function(){
