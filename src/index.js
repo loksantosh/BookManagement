3327103f6dbc7e50032109b7cf20c1efe99f39b1
dhirajuranium <dhirajpatil2311@gmail.com>
2022-07-06 12:01:55 +0530

    index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./routes/route.js');
+const app =express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/group24Database?retryWrites=true&w=majority"
+,{useNewUrlParser: true})
+.then(()=>console.log('MongoDB is connected'))
+.catch(err => console.log(err));
+
+app.use('/',route);
+app.listen(process.env.PORT || 3000, function(){
+    console.log('express app running on PORT '+(process.env.PORT || 3000))
+});
